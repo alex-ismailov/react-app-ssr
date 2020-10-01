433ee33dc6f542b1d12f1b324445ce29bea9882e
smlnx <alexander.is.dev@gmail.com>
2020-10-01 12:16:13 +0300

    add(src/) Client.tsx

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from 'App'
 
-ReactDOM.render(<h1>Woohoo!</h1>, document.getElementById('root'));
+const entryBlock = document.getElementById('root')
+const renderFunction: ReactDOM.Renderer = entryBlock.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render
+
+renderFunction(<BrowserRouter>
+    <App />
+</BrowserRouter>, entryBlock)
